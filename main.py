82bb0103eec309445a4379c0305a949adf0a3590
kassyiemobi <omeh.kasie@gmail.com>
2020-03-06 20:51:15 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+#to build a program on a tesla that checks the age of a User using conditional  logic
+age= input("what is your age?")
+
+if int (age) >18:
+    print("awesome!powering on,Enjoy your ride.")
+elif int (age)== 18:
+    print("congratulations! on your first year of driving")
+
